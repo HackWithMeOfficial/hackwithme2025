1857e5954c46e3210062a607e04357ab2279d49d
Shyaka Chaste <liochastej@gmail.com>
2025-02-15 03:00:03 +0200

    updated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoonPage from './components/ComingSoonPage'
+
+const App = () => {
+  return (
+    <div>
+      <ComingSoonPage />
+    </div>
+  )
+}
+
+export default App
