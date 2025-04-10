ee6f7743180e1a23731570a74423bbe58a610350
Shyaka Chaste <liochastej@gmail.com>
2025-04-11 01:54:07 +0200

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Footer.css';
-import { FaInstagram, FaGithub, FaLinkedin, FaHeart, FaDiscord } from 'react-icons/fa';
+import { FaInstagram, FaGithub, FaLinkedin, FaDiscord } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Hack With Me. All rights reserved.</p>
+      <p className="fiscal-note">
+        Hack With Me is fiscally sponsored by The Hack Foundation, a 501(c)(3) nonprofit with the EIN 81-2908499.
+      </p>
+      
       <div className="social-icons">
         <a href="https://www.instagram.com/hackwithme_official" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
           <FaInstagram className="icon instagram-icon" />
@@ -17,13 +20,10 @@ const Footer = () => {
           <FaLinkedin className="icon linkedin-icon" />
         </a>
         <a href="https://discord.gg/rpsY54CDjF" aria-label="Email" target="_blank" rel="noopener noreferrer">
-          <FaDiscord  className="icon email-icon" />
+          <FaDiscord className="icon email-icon" />
         </a>
       </div>
 
-      <p className="created-by">
-        Created with <FaHeart className="heart-icon" /> by <a href="https://github.com/shyakachaste" target="_blank" rel="noopener noreferrer">Shyaka Chaste</a>
-      </p>
     </footer>
   );
 };
