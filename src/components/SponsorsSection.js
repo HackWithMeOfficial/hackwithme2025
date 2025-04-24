9ee28edd85f4d44fc21778c71a4f23f4c35abebe
Shyaka Chaste <liochastej@gmail.com>
2025-04-24 21:49:35 +0200

    updated

--- src/components/SponsorsSection.js
+++ src/components/SponsorsSection.js
@@ -4,12 +4,16 @@ import sponsorLogo1 from '../assets/sponsors/codecrafters.png';
 import sponsorLogo2 from '../assets/sponsors/xyz.png';
 import sponsorLogo3 from '../assets/sponsors/interviewcake.png';
 import sponsorLogo4 from '../assets/sponsors/wolfram.png';
+import sponsorLogo5 from '../assets/sponsors/thelinuxfoundation.png';
 
 const SponsorsSection = () => {
   return (
     <section className="sponsors-section" id="sponsors">
       <h2>Sponsors</h2>
       <div className="sponsors-grid">
+      <a href="https://www.linuxfoundation.org/" target="_blank" rel="noopener noreferrer" aria-label="The Linux Foundation">
+          <img src={sponsorLogo5} alt="The Linux Foundation logo" className="sponsor-logo" loading="lazy" />
+        </a>
         <a href="https://www.wolfram.com/" target="_blank" rel="noopener noreferrer" aria-label="Wolfram">
           <img src={sponsorLogo4} alt="Wolfram logo" className="sponsor-logo" loading="lazy" />
         </a>
